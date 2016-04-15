f71a628fbda3c86582b04526d2d6f4d7effcd974
xiuheng chen <chenxiuheng@gmail.com>
2016-04-15 14:29:14 +0800

    新特性：从其他服务器拉流，并提供直播服务器

---
+++ src/test/java/com/sengled/cloud/mediaserver/url/URLObjectTest.java
@@ -0,0 +1,36 @@
+package com.sengled.cloud.mediaserver.url;
+
+import java.net.MalformedURLException;
+import java.text.ParseException;
+
+import junit.framework.TestCase;
+
+import com.sengled.cloud.mediaserver.url.URLObject;
+
+public class URLObjectTest extends TestCase {
+    public static void testSimple() throws MalformedURLException, ParseException {
+        String url = "rtsp://user:password@127.0.0.1:5454/path/proxh";
+        URLObject obj = new URLObject(url);
+
+        assertEquals("rtsp", obj.getScheme());
+        assertEquals("user", obj.getUser());
+        assertEquals("password", obj.getPassword());
+        assertEquals("127.0.0.1", obj.getHost());
+        assertEquals(5454, obj.getPort());
+        assertEquals("/path/proxh", obj.getUri());
+    }
+
+    public static void testDefaultPort() throws MalformedURLException, ParseException {
+        String url = "rtsp://user:password@127.0.0.1/path/proxh";
+        URLObject obj = new URLObject(url);
+
+        assertEquals(554, obj.getPort());
+    }
+    
+    public static void testWithParams() throws MalformedURLException, ParseException {
+        String url = "rtsp://user:password@127.0.0.1/path/proxh?a=b";
+        URLObject obj = new URLObject(url);
+
+        assertEquals("/path/proxh", obj.getUri());
+    }
+}
